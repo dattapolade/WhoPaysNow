25967d17b37cb7c3bfc4174d9d63b1c79af29f13
dattapolade <dattatraybpolade1506@gmail.com>
2024-03-31 00:08:58 +0530

    added main logic code for choose to who will be pay the payment

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def choose:
-    try:
-        pass
-    catch:
-        pass
 
+import random
+
+names_string = input("Input everyone's name, separated by a comma. ")
+names = names_string.split(", ")
+
+num_items = len(names)
+
+random_int = random.randint(0, num_items-1)
+
+person_name = names[random_int]
+
+print(f"{person_name} is going to buy the meal today!")
